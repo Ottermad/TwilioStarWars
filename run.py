bbd6637eec5497994ea3c3698be749c7a1b2077d
Charlie Thomas <charlie.thomas@attwoodthomas.net>
2014-10-06 17:56:20 +0100

    init

---
+++ run.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, redirect
+import twilio.twiml
+ 
+app = Flask(__name__)
+ 
+ 
+@app.route("/", methods=['GET', 'POST'])
+def hello_monkey():
+    """Say a caller's name, and play an MP3 for them."""
+ 
+    resp = twilio.twiml.Response()
+    # Greet the caller by name
+    resp.say("Hello ")
+    # Play an MP3
+    resp.play("http://demo.twilio.com/hellomonkey/monkey.mp3")
+ 
+    return str(resp)
+ 
+if __name__ == "__main__":
+    app.run(debug=True)
