3e48727e915248964e6d8a9279fefc08139a2e0b
Charlie Thomas <charlie.thomas@attwoodthomas.net>
2014-10-06 18:13:32 +0100

    Edited twiml

--- run.py
+++ run.py
@@ -7,14 +7,13 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
     """Say a caller's name, and play an MP3 for them."""
- 
-    resp = twilio.twiml.Response()
-    # Greet the caller by name
-    resp.say("Hello ")
-    # Play an MP3
-    resp.play("http://demo.twilio.com/hellomonkey/monkey.mp3")
- 
-    return str(resp)
+    return """
+    <?xml version="1.0" encoding="UTF-8"?>
+    <Response>
+        <Say>Hello Curious George</Say>
+        <Play>http://demo.twilio.com/hellomonkey/monkey.mp3</Play>
+    </Response>
+    """
  
 if __name__ == "__main__":
     app.run(debug=True)
